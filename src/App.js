6fe0e3d1e4663cc095eef47f40124b6646c1e76f
Qumber Zaidi <qumberzaidi@Qumbers-Air.fios-router.home>
2022-01-14 19:28:04 -0500

    MAKE th

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, Component} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import GifCard from './components/GifCard'
+import SearchField from './components/SearchFIeld'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+ function App() {
+const[gif, setGif] = useState("Red")
+const[data, setData] = useState([])
+
+/*let link = 'http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=CSIs6swXccz66TXhQ3EXrafhJUu4Lu5n'
+
+const getData = async() => {
+  await axios.get(link)
+  .then(res => {
+    setData(res.data)
+  })
 }
+*/
 
+/*function handleSubmit (event){
+  event.preventDefault()
+  axios.get(`'http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=CSIs6swXccz66TXhQ3EXrafhJUu4Lu5n'`)
+}
+*/
+
+
+
+
+
+//useEffect(() => {
+//getData()
+//}, [])
+
+
+
+
+
+
+  return (
+    <div className = "App" >
+      
+<h1> Welcome to Giffy Express! hello</h1>      
+<form>
+
+<label>
+GIFS: 
+<input type ="text" name= "gif" />
+</label>
+<input type= " Search" value ="Search" /> 
+
+</form>
+// props
+<GifCard gif={gif} />
+
+
+
+
+    </div>
+  )
+
+
+}
 export default App;
---
+++ src/components/GifCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function GifCard(props) {
+    //destructure props
+const {gif} = props
+
+
+ return(
+<>
+
+
+<h2> THIS IS GIFCARD COMPONENT!!! {gif}</h2>
+
+
+</>
+
+
+)   
+}
+
+export default GifCard
